a6ce127e4ecc506e6ff2dd210ab2ecde41bef679
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2022-09-09 00:05:30 +0500

    init

--- imports/server/main.js
+++ imports/server/main.js
@@ -8,44 +8,46 @@ import uploadFileToS3 from '/imports/lib/uploadFileToS3';
 import { textractFile, getFileAnalysis } from '/imports/lib/textractFile';
 
 Meteor.startup(() => {
-  /**
-   * Upload Pending Files to S3
-   */
-  const pendingFiles = imagesCollection.find({ s3_uploaded: { $eq: null } });
-  pendingFiles.forEach((file) => {
-    try {
-      uploadFileToS3(file);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  Meteor.setInterval(function () {
+    /**
+     * Upload Pending Files to S3
+     */
+    const pendingFiles = imagesCollection.find({ s3_uploaded: { $eq: null } });
+    pendingFiles.forEach((file) => {
+      try {
+        uploadFileToS3(file);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
-  /**
-   * Apply Textract Job to Pending files at S3
-   */
-  const textractFiles = imagesCollection.find({
-    textracted: { $eq: null },
-    analysed: { $eq: null },
-  });
-  textractFiles.forEach((file) => {
-    try {
-      textractFile(file);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    /**
+     * Apply Textract Job to Pending files at S3
+     */
+    const textractFiles = imagesCollection.find({
+      textracted: { $eq: null },
+      analysed: { $eq: null },
+    });
+    textractFiles.forEach((file) => {
+      try {
+        textractFile(file);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
-  /**
-   * Fetch Textract File Analusis
-   */
-  const textractFileAnalysis = imagesCollection.find({
-    analysis: { $eq: null },
-  });
-  textractFileAnalysis.forEach((file) => {
-    try {
-      getFileAnalysis(file);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    /**
+     * Fetch Textract File Analusis
+     */
+    const textractFileAnalysis = imagesCollection.find({
+      analysis: { $eq: null },
+    });
+    textractFileAnalysis.forEach((file) => {
+      try {
+        getFileAnalysis(file);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }, 3000);
 });
