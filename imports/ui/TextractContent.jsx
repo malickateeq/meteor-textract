e3d80f788f4e1374c7b8a61635dbbf9c7c4bedc6
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2022-09-09 18:14:24 +0500

    Files Auto analysis and submission

--- imports/ui/TextractContent.jsx
+++ imports/ui/TextractContent.jsx
@@ -3,24 +3,13 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import imagesCollection from "../db/imagesCollection";
 
-let getAnalysisInterval = null;
-
 export const TextractContent = () => {
     
-    const forceSubmitForTextract = (lastestFile) => {
-        console.log("Textracting...");
-        console.log(lastestFile.name);
-        Meteor.call('files.submit.textract', lastestFile);
-    };
-    
-    const forceGetResults = (lastestFile) => {
-        console.log("Getting Results...");
-        console.log("Analysing...", lastestFile.name);
-        Meteor.call('files.submit.analysis', lastestFile);
-    };
+    let textractSubmissionInterval = null;
+    let textractResultInterval = null;
 
+    // Listener
     const { lastestFile } = useTracker(() => {
-
         let lastestFile = null;
         const noDataAvailable = { files: [], pendingTasksCount: 0 };
         const handler = Meteor.subscribe('files.images.all');
@@ -28,20 +17,43 @@ export const TextractContent = () => {
             return { ...noDataAvailable, isLoading: true };
         }
         const files = imagesCollection.findOne({}).fetch();
+
+        // Clear recent intervals
+        Meteor.clearInterval(textractSubmissionInterval);
+        Meteor.clearInterval(textractResultInterval);
         if(files) {
-            lastestFile = files[0]; 
-            if(!lastestFile.analysis) {
-                setTimeout(() => {
-                    console.log("Analysing...", lastestFile.name);
+            lastestFile = files[0];
+
+            // When the textract submission is incomplete
+            if(!lastestFile.textracted && lastestFile.s3_uploaded) {
+                textractSubmissionInterval = Meteor.setInterval(() => {
+                    console.log("Re-submiting file to textract..");
+                    Meteor.call('files.submit.textract', lastestFile);
+                }, 9000);
+            }
+
+            // If analysis haven't received yet
+            if(!lastestFile.analysis && lastestFile.textracted) {
+                textractResultInterval = Meteor.setInterval(() => {
+                    console.log("Requesting for results..");
                     Meteor.call('files.submit.analysis', lastestFile);
                 }, 3000);
             }
+
+            if(lastestFile?.textracted) {
+                Meteor.clearInterval(textractSubmissionInterval);
+            } 
+            if(lastestFile?.analysis) {
+                Meteor.clearInterval(textractSubmissionInterval);
+                Meteor.clearInterval(textractResultInterval);
+            } 
 
             return { lastestFile: files[0] };
         }
         return { ...noDataAvailable, isLoading: true };
     });
 
+    // Render PDF Content
     const getPDFContent = (analysis) => {
         let content = '';
         analysis.forEach(finding => {
@@ -50,32 +62,52 @@ export const TextractContent = () => {
                     content += "\n";
                 }
                 content += finding.Text;
-            }
-            
+            } 
         });
         return content;
+    };
+
+    const getFileProcessingStatus = (lastestFile) => {
+        if(!lastestFile)
+            return 'Please Upload a file!'
+        else if (!lastestFile.s3_uploaded)
+            return 'Uploading file to S3 Bucket...';
+        else if (!lastestFile.textracted)
+            return 'Submitting file to Textract...';
+        else if (!lastestFile.analysis)
+            return 'Waiting for file results...';
+    };
+
+    const pdfFileProcessingLifecycle = (lastestFile) => {
+        return (
+            <>
+                { !lastestFile?.analysis ? (
+                    <>
+                        <div className="d-flex justify-content-center">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                        <span className='d-flex justify-content-center mt-2'> 
+                            { getFileProcessingStatus(lastestFile) }
+                        </span>
+                    </>
+                ) : getPDFContent(lastestFile.analysis) }
+                
+            </>
+        );
     };
 
   return (
         <>
             <h2 className="fw-bold lh-1 mb-3">Textract Content:</h2>
-            <p className="col-lg-10">
-                { !lastestFile ? 'Please Upload a file!' : (
-                    !lastestFile.s3_uploaded ? 'Uploading file to S3 Bucket...' : (
-                        !lastestFile.textracted ? 'Submitting file to Textract...' : (
-                            !lastestFile.analysis ? 'Waiting for file results...' : (
-                                getPDFContent(lastestFile.analysis)
-                            )
-                        )
-                    )
-                )}
-            </p>
+
+            { pdfFileProcessingLifecycle(lastestFile) }
+            
             <hr className="my-4" />
             <small className="text-muted"> File: { lastestFile?.name } </small>
             <br />
             <br />
-            { lastestFile && !lastestFile?.textracted ? <small className="text-muted"> <button onClick={ (e) => forceSubmitForTextract(lastestFile)} className="btn btn-sm btn-primary mr-3">Submit Again</button>  </small> : "" }
-            { lastestFile && lastestFile?.textracted && !lastestFile?.analysis ? <small className="text-muted"> <button onClick={ (e) => forceGetResults(lastestFile)} className="btn btn-sm btn-primary">Fetch Results</button>  </small> : '' }
         </>
   );
 };
